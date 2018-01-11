22c170dad2b74637f0e04a418e2aa73682803a11
Drew Tucker and Victor Felix <victor@>
2018-01-11 13:46:14 -0800

    Add more functionality to enter inches form, now will display error if field is undefined

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
   $("#heightForm").submit(function(event) {
     event.preventDefault();
     var askHeight = parseInt($("input#heightInput").val());
-    if (askHeight < 48) {
+    console.log(askHeight);
+    if (isNaN(askHeight) || askHeight === 0) {
+      return alert("Please enter a valid number.");
+      $("#merry-go-round").hide();
+      $("#roller-coaster").hide();
+      $("#extreme-coaster").hide();
+    }
+    else if (askHeight < 48) {
       $("#merry-go-round").show();
       $("#roller-coaster").hide();
       $("#extreme-coaster").hide();
@@ -13,7 +20,7 @@ $(document).ready(function() {
       $("#roller-coaster").show();
       $("#extreme-coaster").hide();
     }
-    else {
+    else if (askHeight > 1 || askHeight < 84) {
       $("#merry-go-round").show();
       $("#roller-coaster").show();
       $("#extreme-coaster").show();
